4e58cc2742fdadbb85b074438cdc08df284fc462
Edward Wong <edwardwong@Edwards-MacBook-Pro-3.local>
2018-03-02 12:34:24 -0800

    updated main

--- convnet.py
+++ convnet.py
@@ -7,6 +7,7 @@ from torch.autograd import Variable
 from lib import *
 from networks import *
 import torch.optim as optim
+import matplotlib.pyplot as plt
 
 def accuracy(x, y, net):
     if len(x) != len(y):
@@ -21,19 +22,18 @@ def accuracy(x, y, net):
     return float(correct) / float(len(y))
 
 def train(train_x, train_y, net, full_train_x, full_train_y, test_x, test_y):
-    num_epochs = 7
+    num_epochs = 10
     criterion = nn.CrossEntropyLoss()
     # change optimizer to be what you want
-    #optimizer = optim.Adam(net.parameters(), lr=0.001)
-    optimizer = optim.SGD(net.parameters(), lr = 0.01, momentum=.5)
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
+    #optimizer = optim.SGD(net.parameters(), lr = 0.01)
     training_accuracy = []
     test_accuracy = []
 
     training_size = len(train_x)
     for epoch in range(num_epochs):
-        print("Starting Epoch:", epoch, accuracy(test_x, test_y, net))
-        batch_size = 200
-        for iter_num in range(0, 14):
+        batch_size = 1000
+        for iter_num in range(0, 3):
             batch_x = train_x[batch_size * iter_num : batch_size * (iter_num + 1)]
             batch_y = train_y[batch_size * iter_num : batch_size * (iter_num + 1)]
             inputs, labels = Variable(batch_x), Variable(batch_y)
@@ -45,6 +45,7 @@ def train(train_x, train_y, net, full_train_x, full_train_y, test_x, test_y):
             optimizer.step()
             training_accuracy.append(accuracy(full_train_x, full_train_y, net))
             test_accuracy.append(accuracy(test_x, test_y, net))
+            print("Status Update:", epoch, test_accuracy[-1], training_accuracy[-1])
     return (training_accuracy, test_accuracy)
 
 def main():
@@ -61,13 +62,39 @@ def main():
 
     hard_net = ConvolutionalNet()
 
-    all_X, all_Y = load_both_shape_data("easy_shape_dataset", "hard_shape_dataset")
-    all_X = torch.from_numpy(all_X).float()
-    all_Y = torch.from_numpy(all_Y).long()
-    print(len(all_X), len(all_Y))
+    train_X, train_Y = load_both_shape_data("easy_shape_dataset", "hard_shape_dataset")
+    train_X = torch.from_numpy(train_X).float()
+    train_Y = torch.from_numpy(train_Y).long()
+    print(len(train_X), len(train_Y))
+
+    random_net = ConvolutionalNet()
+
+    test_X, test_Y = load_both_shape_data("test_easy_shape_dataset", "test_hard_shape_dataset")
+    test_X = torch.from_numpy(test_X).float()
+    test_Y = torch.from_numpy(test_Y).long()
+    print(len(test_X), len(test_Y))
 
     print("Training Easy")
-    (easy_training_accuracy, easy_test_accuracy) = train(easy_X, easy_Y, easy_net, all_X, all_Y, all_X, all_Y)
+    (easy_training_accuracy, easy_test_accuracy) = train(easy_X, easy_Y, easy_net, train_X, train_Y, test_X, test_Y)
     print("Training Hard")
-    (hard_training_accuracy, hard_test_accuracy) = train(hard_X, hard_Y, hard_net, all_X, all_Y, all_X, all_Y)
+    (hard_training_accuracy, hard_test_accuracy) = train(hard_X, hard_Y, hard_net, train_X, train_Y, test_X, test_Y)
+    print("Training Random")
+    (random_training_accuracy, random_test_accuracy) = train(train_X, train_Y, random_net, train_X, train_Y, test_X, test_Y)
+
+    f_0 = plt.figure(0)
+    f_0.canvas.set_window_title("Training Accuracy")
+    plt.plot(easy_training_accuracy)
+    plt.plot(hard_training_accuracy)
+    plt.plot(random_training_accuracy)
+    plt.legend(['Easy Shapes Only', 'Hard Shapes Only', 'Easy + Hard Shapes'], loc='best')
+    plt.ylabel('Accuracy')
+
+    f_0 = plt.figure(1)
+    f_0.canvas.set_window_title("Test Accuracy")
+    plt.plot(easy_test_accuracy)
+    plt.plot(hard_test_accuracy)
+    plt.plot(random_test_accuracy)
+    plt.legend(['Easy Shapes Only', 'Hard Shapes Only', 'Easy + Hard Shapes'], loc='best')
+    plt.ylabel('Accuracy')
+    plt.show()    
 main()
