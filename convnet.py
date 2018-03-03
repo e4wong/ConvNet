7e8eb527ebe14997c4b1fe970f5555703b415f90
Edward Wong <edwardwong@Edwards-MacBook-Pro-3.local>
2018-03-02 18:01:55 -0800

    Testing with just hard shapes now

--- convnet.py
+++ convnet.py
@@ -22,14 +22,15 @@ def accuracy(x, y, net):
     return float(correct) / float(len(y))
 
 def train(train_x, train_y, net, full_train_x, full_train_y, test_x, test_y):
-    num_epochs = 10
+    num_epochs = 50
     criterion = nn.CrossEntropyLoss()
     # change optimizer to be what you want
     optimizer = optim.Adam(net.parameters(), lr=0.001)
     #optimizer = optim.SGD(net.parameters(), lr = 0.01)
     training_accuracy = []
     test_accuracy = []
-
+    training_accuracy.append(accuracy(full_train_x, full_train_y, net))
+    test_accuracy.append(accuracy(test_x, test_y, net))
     training_size = len(train_x)
     for epoch in range(num_epochs):
         batch_size = 1000
@@ -43,9 +44,9 @@ def train(train_x, train_y, net, full_train_x, full_train_y, test_x, test_y):
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
-            training_accuracy.append(accuracy(full_train_x, full_train_y, net))
-            test_accuracy.append(accuracy(test_x, test_y, net))
-            print("Status Update:", epoch, test_accuracy[-1], training_accuracy[-1])
+        training_accuracy.append(accuracy(full_train_x, full_train_y, net))
+        test_accuracy.append(accuracy(test_x, test_y, net))
+        print("Status Update:", epoch, test_accuracy[-1], training_accuracy[-1])
     return (training_accuracy, test_accuracy)
 
 def main():
@@ -69,17 +70,17 @@ def main():
 
     random_net = ConvolutionalNet()
 
-    test_X, test_Y = load_both_shape_data("test_easy_shape_dataset", "test_hard_shape_dataset")
+    test_X, test_Y = load_shape_data("test_hard_shape_dataset")
     test_X = torch.from_numpy(test_X).float()
     test_Y = torch.from_numpy(test_Y).long()
     print(len(test_X), len(test_Y))
 
     print("Training Easy")
-    (easy_training_accuracy, easy_test_accuracy) = train(easy_X, easy_Y, easy_net, train_X, train_Y, test_X, test_Y)
+    (easy_training_accuracy, easy_test_accuracy) = train(easy_X, easy_Y, easy_net, hard_X, hard_Y, test_X, test_Y)
     print("Training Hard")
-    (hard_training_accuracy, hard_test_accuracy) = train(hard_X, hard_Y, hard_net, train_X, train_Y, test_X, test_Y)
+    (hard_training_accuracy, hard_test_accuracy) = train(hard_X, hard_Y, hard_net, hard_X, hard_Y, test_X, test_Y)
     print("Training Random")
-    (random_training_accuracy, random_test_accuracy) = train(train_X, train_Y, random_net, train_X, train_Y, test_X, test_Y)
+    (random_training_accuracy, random_test_accuracy) = train(train_X, train_Y, random_net, hard_X, hard_Y, test_X, test_Y)
 
     f_0 = plt.figure(0)
     f_0.canvas.set_window_title("Training Accuracy")
